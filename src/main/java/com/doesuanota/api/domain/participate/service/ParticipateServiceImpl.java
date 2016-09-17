2e27c11df421dbc409a224d51c01f1b9de196fd3
matheusmessora <matheus.messora.vpn@gmail.com>
2016-09-17 00:16:09 -0300

    testing github protected branch

--- src/main/java/com/doesuanota/api/domain/participate/service/ParticipateServiceImpl.java
+++ src/main/java/com/doesuanota/api/domain/participate/service/ParticipateServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class ParticipateServiceImpl implements ParticipateService {
 
+
     public Participant register(final Participant participant) {
         return null;
     }
