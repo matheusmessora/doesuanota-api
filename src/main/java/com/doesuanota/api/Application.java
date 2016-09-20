fa37cc8b027139f5bb2ab4a932708d74d5cbb4cc
matheusmessora <matheus.messora.vpn@gmail.com>
2016-09-20 00:21:26 -0300

    [TEST] added spring boot CORS configuration. Fixing nginx CORS OPTIONS method

--- src/main/java/com/doesuanota/api/Application.java
+++ src/main/java/com/doesuanota/api/Application.java
@@ -2,7 +2,11 @@ package com.doesuanota.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan
 @EnableAutoConfiguration
@@ -10,5 +14,15 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://doesuanota.com.br");
+            }
+        };
     }
 }
